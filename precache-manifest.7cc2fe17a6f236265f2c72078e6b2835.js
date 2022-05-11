b4f78b5b91466bcce2ee264c6187a46991605715
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2022-05-11 17:18:08 +0300

    Updates

---
+++ precache-manifest.7cc2fe17a6f236265f2c72078e6b2835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162bc44e6828f55cc95347fbaddd0f5f",
+    "url": "/IT-Kamasutra_Todolist/index.html"
+  },
+  {
+    "revision": "6745d2df7701850b80a7",
+    "url": "/IT-Kamasutra_Todolist/static/css/main.c3b5e57e.chunk.css"
+  },
+  {
+    "revision": "fe1c1542cce1b4585cbd",
+    "url": "/IT-Kamasutra_Todolist/static/js/2.31d476de.chunk.js"
+  },
+  {
+    "revision": "3cb93fb21b1482abba2645a922a6353a",
+    "url": "/IT-Kamasutra_Todolist/static/js/2.31d476de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6745d2df7701850b80a7",
+    "url": "/IT-Kamasutra_Todolist/static/js/main.49339641.chunk.js"
+  },
+  {
+    "revision": "7552f943aff433be5b85",
+    "url": "/IT-Kamasutra_Todolist/static/js/runtime-main.caaa511a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IT-Kamasutra_Todolist/precache-manifest.7cc2fe17a6f236265f2c72078e6b2835.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IT-Kamasutra_Todolist/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
